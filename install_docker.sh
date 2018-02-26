53a1f26fc1f8c1ceae17477d6a3b953d1a91ee66
Umamaheshwar <urao@juniper.net>
2018-02-26 13:50:44 -0800

    Modified

--- install_docker.sh
+++ install_docker.sh
@@ -1,42 +1,48 @@
 #!/usr/bin/env bash
+# Tested with Ubuntu 16.04 and Centos 7.4
 set -e
 
 EXITCODE=0
 
-deploy_on_ubuntu() {
+function deploy_on_ubuntu ()
+{
    echo "Deploying docker on Ubuntu...."
    echo "First, remove existing version of docker"
    sudo apt-get remove -y docker docker-engine docker.io
    rm -rf /var/lib/docker
 
    echo "start installing.."
-   sudo apt-get update
+   sudo apt-get update -y
    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
-   sudo apt-get update
+   sudo apt-get update -y
    sudo apt-get install docker-ce
 
    echo "verify if docker installed correctly or not"
    sudo docker run hello-world
    RETVAL=$?
    if [ !$RETVAL ]; then
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
       echo "Successfully deployed docker!!!!"
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
    else
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
       echo "FAILED to deploy docker @@@@@"
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
       EXITCODE=1
    fi
 }
 
-deploy_on_centos() {
+function deploy_on_centos ()
+{
    echo "Deploying docker on Centos...."
    echo "First, remove existing version of docker"
    sudo yum remove -y docker docker-common docker-selinux docker-engine
    rm -rf /var/lib/docker
-   
+
    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-   sudo yum update
+   sudo yum update -y
    sudo yum install -y docker-ce
 
    sudo systemctl start docker
@@ -44,21 +50,23 @@ deploy_on_centos() {
    sudo docker run hello-world
    RETVAL=$?
    if [ !$RETVAL ]; then
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
       echo "Successfully deployed docker!!!!"
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
    else
-      echo "FAILED to deploy docker @@@@@"
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+      echo "FAILED FAILED to deploy docker !"
+      echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
       EXITCODE=1
    fi
 }
 
 
 if [ -f /etc/lsb-release ]; then
-   deploy_on_ubuntu()
+   deploy_on_ubuntu
 elif [ -f /etc/redhat-release ]; then
-   deploy_on_centos()
+   deploy_on_centos
 else
    echo "Unsupported OS for now !!!"
    EXITCODE=1
 fi
-
-exit $EXITCODE
