a5564f0f13fcbf8f136a38a6ad8387eedcfb60ac
umamaheshwararao manekar <urao@juniper.net>
2018-02-26 23:46:07 -0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -16,7 +16,7 @@ function deploy_on_ubuntu ()
    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
    sudo apt-get update -y
-   sudo apt-get install docker-ce
+   sudo apt-get install -y docker-ce wget telnet
 
    echo "verify if docker installed correctly or not"
    sudo docker run hello-world
@@ -43,7 +43,7 @@ function deploy_on_centos ()
    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
    sudo yum update -y
-   sudo yum install -y docker-ce
+   sudo yum install -y docker-ce wget telnet
 
    sudo systemctl start docker
    echo "verify if docker installed correctly or not"
